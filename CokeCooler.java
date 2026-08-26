8861bc11da481fb340529c8009e522c4d77d99bf
BenC <ben2009002@gmail.com>
2026-08-26 12:26:17 -0700

    Aug26Wed

--- CokeCooler.java
+++ CokeCooler.java
@@ -1,3 +1,24 @@
 public class CokeCooler {
-    
+    private int amount;
+
+    public CokeCooler(){
+        amount = 100;
+    }
+
+    public CokeCooler(int a){
+        amount = a;
+    }
+
+    public void drink(int amt){
+        amount -= amt;
+    }
+
+    public void refill(int amt){
+        amount += amt;
+    }
+
+    public void cokeCount(){
+        System.out.println(amount);
+    }
+
 }
---
+++ CokeCoolerMain.java
@@ -0,0 +1,12 @@
+public class CokeCoolerMain {
+    public static void main(String[] args) {
+        CokeCooler c = new CokeCooler();
+        CokeCooler c1 = new CokeCooler();
+
+        c.refill(20);
+        c.drink(10);
+        c.cokeCount();
+
+
+    }
+}
