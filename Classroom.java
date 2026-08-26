3f69350c9ebfc4bdcc51847f9f321af443ac6eb8
BenC <ben2009002@gmail.com>
2026-08-26 12:05:00 -0700

    CandyCW

---
+++ Classroom.java
@@ -0,0 +1,39 @@
+public class Classroom{ 
+    private String teacherName;
+    private int numDesks;
+    private int numStudents;
+
+    public Classroom(String teacherName, int numDesks, int numStudents){ 
+        this.teacherName = teacherName;
+        this.numDesks = numDesks;
+        this.numStudents = numStudents;
+    }
+    
+    public Classroom(String teacherName){
+        this(teacherName, 0, 0);
+    }
+
+    public int desks(){
+        return this.numDesks;
+    }
+
+    public String teacher(){
+        return this.teacherName;
+    }
+
+    public int students(){
+        return this.numStudents;
+    }
+
+    public void setDesks(int amt){
+        this.numDesks = amt;
+    }
+
+    public void setTeacher(String teach){
+        this.teacherName = teach;
+    }
+
+    public void setStudents(int amt){
+        this.numStudents = amt;
+    }
+}
---
+++ ClassroomMain.java
@@ -0,0 +1,5 @@
+public class ClassroomMain {
+    public static void main(String[] args) {
+        
+    }
+}
